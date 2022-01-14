bbec27bc1d9e15a5b2f31aabfd2fa0e6c9ab2dcc
Arthur-Clarke <71024137+Arthur-Clarke@users.noreply.github.com>
2022-01-14 19:59:19 -0300

    Add files via upload

--- projeto-27/PRO--C27-Template-main/sketch.js
+++ projeto-27/PRO--C27-Template-main/sketch.js
@@ -39,7 +39,7 @@ function setup() {
 function keyPressed() {
 	if (keyCode === UP_ARROW){
 		
-		Matter.Body.applyForce(bobObject1.Body,bobObject1.Body.position,{x:-50,y:-45});
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45});
 		}
 	}
 
